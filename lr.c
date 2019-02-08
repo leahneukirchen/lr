40b4fe60da9d1d27abb715f4a31c0effcbc7687c
Leah Neukirchen <leah@vuxu.org>
2019-02-08 18:30:23 +0100

    update copyright years

--- lr.c
+++ lr.c
@@ -1,7 +1,7 @@
 /* lr - a better recursive ls/find */
 
 /*
- * Copyright (C) 2015-2018 Leah Neukirchen <purl.org/net/chneukirchen>
+ * Copyright (C) 2015-2019 Leah Neukirchen <purl.org/net/chneukirchen>
  * Parts of code derived from musl libc, which is
  * Copyright (C) 2005-2014 Rich Felker, et al.
  *
